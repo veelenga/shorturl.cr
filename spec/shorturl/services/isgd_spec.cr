6355cf8073cd473d3ce4a23a50bf73e87a8d7af1
veelenga <velenhaupt@gmail.com>
2015-09-21 17:51:08 +0300

    Installation info and spec correction

--- spec/shorturl/services/isgd_spec.cr
+++ spec/shorturl/services/isgd_spec.cr
@@ -4,11 +4,11 @@ module ShortURL
   module Services
     describe Isgd do
       it "should have valid service characteristics" do
-        isgd = Isgd.new
-        isgd.hostname.should eq "is.gd"
-        isgd.method.should eq :get
-        isgd.action.should eq "/api.php"
-        isgd.param.should eq "longurl"
+        service = Isgd.new
+        service.hostname.should eq "is.gd"
+        service.method.should eq :get
+        service.action.should eq "/api.php"
+        service.param.should eq "longurl"
       end
     end
   end
--- src/shorturl/version.cr
+++ src/shorturl/version.cr
@@ -1,3 +1,3 @@
 module ShortURL
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 end
